e4ec252eaae3f25c6e5685e5ad95058e3b6c6813
Shawn Abramson <3913128+shawnabramson@users.noreply.github.com>
2024-04-09 10:57:20 -0400

    Initialize lab-exercise-1

---
+++ blocks/product-details/product-details.js
@@ -0,0 +1,55 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-extraneous-dependencies */
+
+// Drop-in Tools
+import { initializers } from '@dropins/tools/initializer.js';
+
+// Drop-in APIs
+import * as product from '@dropins/storefront-pdp/api.js';
+import { addProductsToCart } from '@dropins/storefront-cart/api.js';
+
+// Drop-in Providers
+import { render as productRenderer } from '@dropins/storefront-pdp/render.js';
+
+// Drop-in Containers
+import ProductDetails from '@dropins/storefront-pdp/containers/ProductDetails.js';
+
+// Libs
+import { getConfigValue } from '../../scripts/configs.js';
+import { getSkuFromUrl } from '../../scripts/commerce.js';
+
+export default async function decorate(block) {
+  // Initialize Drop-ins
+  initializers.register(product.initialize, {});
+
+  // Set Fetch Endpoint (Service)
+  product.setEndpoint(await getConfigValue('commerce-endpoint'));
+
+  // Set Fetch Headers (Service)
+  product.setFetchGraphQlHeaders({
+    'Content-Type': 'application/json',
+    'Magento-Environment-Id': await getConfigValue('commerce-environment-id'),
+    'Magento-Website-Code': await getConfigValue('commerce-website-code'),
+    'Magento-Store-View-Code': await getConfigValue('commerce-store-view-code'),
+    'Magento-Store-Code': await getConfigValue('commerce-store-code'),
+    'Magento-Customer-Group': await getConfigValue('commerce-customer-group'),
+    'x-api-key': await getConfigValue('commerce-x-api-key'),
+  });
+
+  // Render Containers
+  return productRenderer.render(ProductDetails, {
+    sku: getSkuFromUrl(),
+    carousel: {
+      controls: 'thumbnailsColumn', // 'thumbnailsColumn', 'thumbnailsRow', 'dots'
+      mobile: true,
+    },
+    onAddToCart: (args) => {
+      // eslint-disable-next-line no-console
+      console.debug('onAddToCart', args);
+      addProductsToCart([args]);
+    },
+    slots: {
+      // ???? 🔮 What could this be?
+    },
+  })(block);
+}
